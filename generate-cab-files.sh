4afb98f535f060c290cedd2fea77c050d0ae73d6
Alexander Martinz <amartinz@shiftphones.com>
2022-10-06 11:34:45 +0200

    Version: 3.9.20221006
    Carry forward old release notes, until a release actually
    gets publically released to LVFS.
    
    Change-Id: I232ccce67d43665c0b000ce4011651b6d2b4061d
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- generate-cab-files.sh
+++ generate-cab-files.sh
@@ -5,12 +5,13 @@ OUTPUT_DIRECTORY="output"
 
 ###############################
 # Image information (edit this)
-IMAGE_DATE="2022-09-23"
-IMAGE_URL="https://gitlab.shift-gmbh.com/ShiftOSS/android_proprietary_vendor_firmware/-/raw/dbc4f0a12ce894962301c274ded02890ac197a3f/axolotl/radio/abl.img"
-IMAGE_VERSION="3.9.20220923"
+IMAGE_DATE="2022-10-06"
+IMAGE_URL="https://gitlab.shift-gmbh.com/ShiftOSS/android_proprietary_vendor_firmware/-/raw/85d96c00f15ad30b382c7fb237ed019212549d98/axolotl/radio/abl.img"
+IMAGE_VERSION="3.9.20221006"
 IMAGE_DESCRIPTION=$(cat << EOF
 <p>This release brings the following fixes and improvements:</p>
 <ul>
+  <li>Add support for enabling developer mode</li>
   <li>Correct spelling mistakes</li>
   <li>Report ABL version through cmdline</li>
 </ul>
